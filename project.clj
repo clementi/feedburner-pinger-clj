1a6e7740fae00feca51cdb9a549af73a87868538
Jeffrey Pratt <@clementi1832>
2015-03-13 15:13:27 -0600

    Get local Maven repo to work

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.tenfactorial.web.feeds/feedburner-pinger-java "1.0.0"]
-                 [org.apache.httpcomponents/httpclient "4.3.1"]
-                 [com.google.code.gson/gson "2.3.1"]]
-  :repositories {"local" ~(str (.toURI (java.io.File. "maven-repo")))})
+                 [com.tenfactorial.web.feeds/feedburner-pinger-java "1.0.0"]]
+  :plugins [[lein-localrepo "0.5.3"]]
+  :repositories {"local" "file:maven-repo"})
