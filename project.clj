cd4295dade5fe7a65a5d07ae68a5e110fe568875
Jeffrey Pratt <@clementi1832>
2015-03-13 12:24:43 -0600

    Enable interop

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [feedburner-pinger "1.0.0"]])
+                 [com.tenfactorial.web.feeds/feedburner-pinger-java "1.0.0"]
+                 [org.apache.httpcomponents/httpclient "4.3.1"]
+                 [com.google.code.gson/gson "2.3.1"]]
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven-repo")))})
--- src/feedburner_pinger_clj/core.clj
+++ src/feedburner_pinger_clj/core.clj
@@ -1,11 +1,21 @@
 (ns feedburner-pinger-clj.core
-  (:require [feedburner-pinger :as pinger]))
+  (import com.tenfactorial.web.feeds.PingRequest)
+  (import com.tenfactorial.web.feeds.PingResponse)
+  (import com.tenfactorial.web.feeds.HttpPingingClient)
+  (import com.tenfactorial.web.feeds.PingStatus))
 
-(defn ping [url]
-  (let [[request (pinger/PingRequest. url)]
-        [client (pinger/HttpPingingClient.)]]
-    (to-map (.ping client request))))
+(defn- to-keyword [status]
+  (cond
+   (.equals status PingStatus/SUCCEEDED) :succeeded
+   (.equals status PingStatus/THROTTLED) :throttled
+   (.equals status PingStatus/FAILED) :failed
+   :else :unknown))
 
 (defn- to-map [response]
-  {:status (.getStatus response)
+  {:status (to-keyword (.getStatus response))
    :message (.getMessage response)})
+
+(defn ping [url]
+  (let [request (PingRequest. url)
+        client (HttpPingingClient.)]
+    (to-map (.ping client request))))
