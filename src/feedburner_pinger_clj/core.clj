0edf51ce985a8d5c02e194d47a0d4a4bd876aa07
Jeffrey Pratt <@clementi1832>
2015-04-10 12:46:07 -0600

    Use keyword function and string functions to turn response status enum into keyword

--- src/feedburner_pinger_clj/core.clj
+++ src/feedburner_pinger_clj/core.clj
@@ -4,15 +4,8 @@
   (import com.tenfactorial.web.feeds.HttpPingingClient)
   (import com.tenfactorial.web.feeds.PingStatus))
 
-(defn- to-keyword [status]
-  (cond
-   (.equals status PingStatus/SUCCEEDED) :succeeded
-   (.equals status PingStatus/THROTTLED) :throttled
-   (.equals status PingStatus/FAILED) :failed
-   :else :unknown))
-
 (defn- to-map [response]
-  {:status (to-keyword (.getStatus response))
+  {:status (keyword (clojure.string/lower-case (str (.getStatus response))))
    :message (.getMessage response)})
 
 (defn ping [url]
